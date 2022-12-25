3fac8950ea30424c57e810c6e4563611d6feaaef
szbai <szabolcs.nagy.hu@gmail.com>
2022-12-25 12:30:43 +0100

    prettier

--- src/App.js
+++ src/App.js
@@ -5,19 +5,31 @@ import Home from "./routes/home/home.component";
 import Authentication from "./routes/authentication/authentication.components";
 
 const Shop = () => {
-  return <div>shop: to be implemented</div>;
+	return <div>shop: to be implemented</div>;
 };
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/auth" element={<Authentication />} />
-      </Route>
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={<Navigation />}
+			>
+				<Route
+					index
+					element={<Home />}
+				/>
+				<Route
+					path="/shop"
+					element={<Shop />}
+				/>
+				<Route
+					path="/auth"
+					element={<Authentication />}
+				/>
+			</Route>
+		</Routes>
+	);
 };
 
 export default App;
